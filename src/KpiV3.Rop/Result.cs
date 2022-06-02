73564cbac6fd78d98113ae0be9d2ba48339cbefa
Asylkhan Azat <asylkhan.azat@gmail.com>
2022-06-02 19:50:50 +0600

    Add Rop classes

---
+++ src/KpiV3.Rop/Result.cs
@@ -0,0 +1,265 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace KpiV3.Rop;
+
+public readonly struct Result<TSuccess, TFailure>
+{
+    private readonly TSuccess? _success;
+    private readonly TFailure? _failure;
+
+    private Result(
+        TSuccess? success,
+        TFailure? failure,
+        bool isSuccess)
+    {
+        _success = success;
+        _failure = failure;
+
+        IsSuccess = isSuccess;
+    }
+
+    [MemberNotNullWhen(true, nameof(_success))]
+    [MemberNotNullWhen(false, nameof(_failure))]
+    public bool IsSuccess { get; }
+
+    [MemberNotNullWhen(true, nameof(_failure))]
+    [MemberNotNullWhen(false, nameof(_success))]
+    public bool IsFailure => !IsSuccess;
+
+    public TSuccess Success
+    {
+        get
+        {
+            if (!IsSuccess)
+            {
+                throw new InvalidOperationException("Trying access Success property while result is Failed");
+            }
+
+            return _success;
+        }
+    }
+
+    public TFailure Failure
+    {
+        get
+        {
+            if (!IsFailure)
+            {
+                throw new InvalidOperationException("Trying access Failure property while result is Succeed");
+            }
+
+            return _failure;
+        }
+    }
+
+    public bool TryGetSuccess([NotNullWhen(true)] out TSuccess? success)
+    {
+        success = default;
+
+        if (IsSuccess)
+        {
+            success = _success;
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool TryGetFailure([NotNullWhen(true)] out TFailure? failure)
+    {
+        failure = default;
+
+        if (IsFailure)
+        {
+            failure = _failure;
+            return true;
+        }
+
+        return false;
+    }
+
+    public T Match<T>(Func<TSuccess, T> onSuccess, Func<TFailure, T> onFailure)
+    {
+        return IsSuccess ? onSuccess(_success) : onFailure(_failure);
+    }
+
+    public Result<T, TFailure> Map<T>(Func<TSuccess, T> mapper)
+    {
+        return IsSuccess ?
+            Result<T, TFailure>.Ok(mapper(_success)) :
+            Result<T, TFailure>.Fail(_failure);
+    }
+
+    public Result<TSuccess, T> MapFailure<T>(Func<TFailure, T> mapper)
+    {
+        return IsFailure ?
+            Result<TSuccess, T>.Fail(mapper(_failure)) :
+            Result<TSuccess, T>.Ok(_success);
+    }
+
+    public Result<T, TFailure> Bind<T>(Func<TSuccess, Result<T, TFailure>> binder)
+    {
+        return IsSuccess ?
+            binder(_success) :
+            Result<T, TFailure>.Fail(_failure);
+    }
+
+    public Result<TSuccess, T> BindFailure<T>(Func<TFailure, Result<TSuccess, T>> binder)
+    {
+        return IsFailure ?
+            binder(_failure) :
+            Result<TSuccess, T>.Ok(_success);
+    }
+
+    public Result<TFailure> Bind(Func<TSuccess, Result<TFailure>> binder)
+    {
+        return IsSuccess ? binder(_success) : Result<TFailure>.Fail(_failure);
+    }
+
+    public Result<TFailure> BindFailure(Func<TFailure, Result<TFailure>> binder)
+    {
+        return IsFailure ? binder(_failure) : Result<TFailure>.Ok();
+    }
+
+    public Result<TSuccess, TFailure> Tee(Action<TSuccess> onSuccess)
+    {
+        if (IsSuccess)
+        {
+            onSuccess(_success);
+        }
+
+        return this;
+    }
+
+    public Result<TSuccess, TFailure> TeeFailure(Action<TFailure> onFailure)
+    {
+        if (IsFailure)
+        {
+            onFailure(_failure);
+        }
+
+        return this;
+    }
+
+    public Result<TSuccess, TFailure> TeeEither(Action<TSuccess> onSuccess, Action<TFailure> onFailure)
+    {
+        if (IsSuccess)
+        {
+            onSuccess(_success);
+        }
+        else
+        {
+            onFailure(_failure);
+        }
+
+        return this;
+    }
+
+    public static Result<TSuccess, TFailure> Ok(TSuccess success)
+    {
+        return new Result<TSuccess, TFailure>(success, default, true);
+    }
+
+    public static Result<TSuccess, TFailure> Fail(TFailure failure)
+    {
+        return new Result<TSuccess, TFailure>(default, failure, false);
+    }
+}
+
+public readonly struct Result<TFailure>
+{
+    private readonly TFailure? _failure;
+
+    private Result(TFailure failure)
+    {
+        _failure = failure;
+
+        IsFailure = true;
+    }
+
+    [MemberNotNullWhen(true, nameof(_failure))]
+    public bool IsFailure { get; }
+
+    [MemberNotNullWhen(false, nameof(_failure))]
+    public bool IsSuccess => !IsFailure;
+
+    public T Match<T>(Func<T> onSuccess, Func<TFailure, T> onFailure)
+    {
+        return IsSuccess ? onSuccess() : onFailure(_failure);
+    }
+
+    public Result<TSuccess, TFailure> InsertSuccess<TSuccess>(Func<Result<TSuccess, TFailure>> success)
+    {
+        return IsSuccess ? success() : Result<TSuccess, TFailure>.Fail(_failure);
+    }
+
+    public Result<TSuccess, TFailure> InsertSuccess<TSuccess>(Func<TSuccess> onSuccess)
+    {
+        return IsSuccess ?
+            Result<TSuccess, TFailure>.Ok(onSuccess()) :
+            Result<TSuccess, TFailure>.Fail(_failure);
+    }
+
+    public Result<TFailure> Bind(Func<Result<TFailure>> binder)
+    {
+        return IsSuccess ? binder() : Result<TFailure>.Fail(_failure);
+    }
+
+    public Result<T> MapFailure<T>(Func<TFailure, T> mapper)
+    {
+        return IsFailure ?
+            Result<T>.Fail(mapper(_failure)) :
+            Result<T>.Ok();
+    }
+
+    public Result<T> BindFailure<T>(Func<TFailure, Result<T>> binder)
+    {
+        return IsFailure ?
+            binder(_failure) :
+            Result<T>.Ok();
+    }
+
+    public Result<TFailure> Tee(Action onSuccess)
+    {
+        if (IsSuccess)
+        {
+            onSuccess();
+        }
+
+        return this;
+    }
+
+    public Result<TFailure> TeeFailure(Action<TFailure> onFailure)
+    {
+        if (IsFailure)
+        {
+            onFailure(_failure);
+        }
+
+        return this;
+    }
+
+    public Result<TFailure> TeeEither(Action onSuccess, Action<TFailure> onFailure)
+    {
+        if (IsSuccess)
+        {
+            onSuccess();
+        }
+        else
+        {
+            onFailure(_failure);
+        }
+
+        return this;
+    }
+
+    public static Result<TFailure> Ok()
+    {
+        return new Result<TFailure>();
+    }
+
+    public static Result<TFailure> Fail(TFailure failure)
+    {
+        return new Result<TFailure>(failure);
+    }
+}
---
+++ src/KpiV3.Rop/Result_1_Extensions.cs
@@ -0,0 +1,201 @@
+﻿namespace KpiV3.Rop;
+
+public static class Result_1_Extensions
+{
+    public static Task<T> MatchAsync<TFailure, T>(
+        this Result<TFailure> result,
+        Func<Task<T>> onSuccess,
+        Func<TFailure, Task<T>> onFailure)
+    {
+        return result.Match(onSuccess, onFailure);
+    }
+
+    public static Task<T> MatchAsync<TFailure, T>(
+        this Result<TFailure> result,
+        Func<T> onSuccess,
+        Func<TFailure, Task<T>> onFailure)
+    {
+        return result.MatchAsync(() => Task.FromResult(onSuccess()), onFailure);
+    }
+
+    public static Task<T> MatchAsync<TFailure, T>(
+        this Result<TFailure> result,
+        Func<Task<T>> onSuccess,
+        Func<TFailure, T> onFailure)
+    {
+        return result.MatchAsync(onSuccess, failure => Task.FromResult(onFailure(failure)));
+    }
+
+    public static async Task<T> MatchAsync<TFailure, T>(
+        this Task<Result<TFailure>> task,
+        Func<Task<T>> onSuccess,
+        Func<TFailure, Task<T>> onFailure)
+    {
+        var result = await task;
+
+        return await result.MatchAsync(onSuccess, onFailure);
+    }
+
+    public static async Task<T> MatchAsync<TFailure, T>(
+        this Task<Result<TFailure>> task,
+        Func<T> onSuccess,
+        Func<TFailure, Task<T>> onFailure)
+    {
+        var result = await task;
+
+        return await result.MatchAsync(onSuccess, onFailure);
+    }
+
+    public static async Task<T> MatchAsync<TFailure, T>(
+        this Task<Result<TFailure>> task,
+        Func<Task<T>> onSuccess,
+        Func<TFailure, T> onFailure)
+    {
+        var result = await task;
+
+        return await result.MatchAsync(onSuccess, onFailure);
+    }
+
+    public static async Task<T> MatchAsync<TFailure, T>(
+        this Task<Result<TFailure>> task,
+        Func<T> onSuccess,
+        Func<TFailure, T> onFailure)
+    {
+        var result = await task;
+
+        return result.Match(onSuccess, onFailure);
+    }
+
+    public static Task<Result<TFailure>> BindAsync<TFailure>(
+        this Result<TFailure> result,
+        Func<Task<Result<TFailure>>> binder)
+    {
+        return result.MatchAsync(
+            binder,
+            failure => Task.FromResult(Result<TFailure>.Fail(failure)));
+    }
+
+    public static async Task<Result<TFailure>> BindAsync<TFailure>(
+        this Task<Result<TFailure>> task,
+        Func<Task<Result<TFailure>>> binder)
+    {
+        var result = await task;
+
+        return await result.BindAsync(binder);
+    }
+
+    public static async Task<Result<TFailure>> BindAsync<TFailure>(
+        this Task<Result<TFailure>> task,
+        Func<Result<TFailure>> binder)
+    {
+        var result = await task;
+
+        return result.Bind(binder);
+    }
+
+    public static Task<Result<TSuccess, TFailure>> InsertSuccessAsync<TSuccess, TFailure>(
+        this Result<TFailure> result,
+        Func<Task<Result<TSuccess, TFailure>>> binder)
+    {
+        return result.Match(binder, failure => Task.FromResult(Result<TSuccess, TFailure>.Fail(failure)));
+    }
+
+    public static async Task<Result<TSuccess, TFailure>> InsertSuccessAsync<TSuccess, TFailure>(
+        this Task<Result<TFailure>> task,
+        Func<Task<Result<TSuccess, TFailure>>> binder)
+    {
+        var result = await task;
+
+        return await result.InsertSuccessAsync(binder);
+    }
+
+    public static async Task<Result<TSuccess, TFailure>> InsertSuccessAsync<TSuccess, TFailure>(
+        this Task<Result<TFailure>> task,
+        Func<Result<TSuccess, TFailure>> binder)
+    {
+        var result = await task;
+
+        return result.InsertSuccess(binder);
+    }
+
+    public static Task<Result<TSuccess, TFailure>> InsertSuccessAsync<TSuccess, TFailure>(
+        this Result<TFailure> result,
+        Func<Task<TSuccess>> onSuccess)
+    {
+        return result.Match(
+            async () => Result<TSuccess, TFailure>.Ok(await onSuccess()),
+            failure => Task.FromResult(Result<TSuccess, TFailure>.Fail(failure)));
+    }
+
+    public static async Task<Result<TSuccess, TFailure>> InsertSuccessAsync<TSuccess, TFailure>(
+        this Task<Result<TFailure>> task,
+        Func<Task<TSuccess>> onSuccess)
+    {
+        var result = await task;
+
+        return await result.InsertSuccessAsync(onSuccess);
+    }
+
+    public static async Task<Result<TSuccess, TFailure>> InsertSuccessAsync<TSuccess, TFailure>(
+        this Task<Result<TFailure>> task,
+        Func<TSuccess> onSuccess)
+    {
+        var result = await task;
+
+        return result.InsertSuccess(onSuccess);
+    }
+
+    public static Task<Result<T>> MapFailureAsync<TFailure, T>(
+        this Result<TFailure> result,
+        Func<TFailure, Task<T>> mapper)
+    {
+        return result.Match(
+            () => Task.FromResult(Result<T>.Ok()),
+            async failure => Result<T>.Fail(await mapper(failure)));
+    }
+
+    public static async Task<Result<T>> MapFailureAsync<TFailure, T>(
+        this Task<Result<TFailure>> task,
+        Func<TFailure, Task<T>> mapper)
+    {
+        var result = await task;
+
+        return await result.MapFailureAsync(mapper);
+    }
+
+    public static async Task<Result<T>> MapFailureAsync<TFailure, T>(
+        this Task<Result<TFailure>> task,
+        Func<TFailure, T> mapper)
+    {
+        var result = await task;
+
+        return result.MapFailure(mapper);
+    }
+
+    public static Task<Result<T>> BindFailureAsync<TFailure, T>(
+        this Result<TFailure> result,
+        Func<TFailure, Task<Result<T>>> binder)
+    {
+        return result.Match(
+            () => Task.FromResult(Result<T>.Ok()),
+            binder);
+    }
+
+    public static async Task<Result<T>> BindFailureAsync<TFailure, T>(
+        this Task<Result<TFailure>> task,
+        Func<TFailure, Task<Result<T>>> binder)
+    {
+        var result = await task;
+
+        return await result.BindFailureAsync(binder);
+    }
+
+    public static async Task<Result<T>> BindFailureAsync<TFailure, T>(
+        this Task<Result<TFailure>> task,
+        Func<TFailure, Result<T>> binder)
+    {
+        var result = await task;
+
+        return result.BindFailure(binder);
+    }
+}
