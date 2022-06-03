c9bd98d08990d07a128e7fd587722f1048c5bc6a
Asylkhan Azat <asylkhan.azat@gmail.com>
2022-06-03 23:36:51 +0600

    Add BulkRegisterEmployeesCommand

---
+++ src/KpiV3.Domain/Employees/Commands/BulkRegisterEmployeesCommand.cs
@@ -0,0 +1,126 @@
+﻿using KpiV3.Domain.Employees.DataContracts;
+using KpiV3.Domain.Employees.Ports;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace KpiV3.Domain.Employees.Commands;
+
+public record BulkRegisterEmployee
+{
+    [Required]
+    public string Email { get; init; } = default!;
+
+    [Required]
+    public string FirstName { get; init; } = default!;
+    
+    [Required]
+    public string LastName { get; init; } = default!;
+    
+    public string? MiddleName { get; init; }
+    
+    [Required]
+    public string Position { get; init; } = default!;
+}
+
+public record BulkRegisterEmployeesCommand : IRequest<Result<IError>>
+{
+    public List<BulkRegisterEmployee> Employees { get; init; } = default!;
+}
+
+public class BulkRegisterEmployeesCommandHandler : IRequestHandler<BulkRegisterEmployeesCommand, Result<IError>>
+{
+    private readonly IPositionRepository _positionRepository;
+    private readonly IMediator _mediator;
+
+    public BulkRegisterEmployeesCommandHandler(
+        IPositionRepository positionRepository,
+        IMediator mediator)
+    {
+        _positionRepository = positionRepository;
+        _mediator = mediator;
+    }
+
+    public async Task<Result<IError>> Handle(BulkRegisterEmployeesCommand request, CancellationToken cancellationToken)
+    {
+        var context = new BulkContext(_positionRepository, _mediator);
+
+        foreach (var employee in request.Employees)
+        {
+            var result = await context.RegisterEmployeeAsync(employee);
+
+            if (result.IsFailure)
+            {
+                return result;
+            }
+        }
+
+        return Result<IError>.Ok();
+    }
+
+    private class BulkContext
+    {
+        private readonly IPositionRepository _positionRepository;
+        private readonly IMediator _mediator;
+        private readonly Dictionary<string, Guid> _positionIds;
+
+        public BulkContext(
+            IPositionRepository positionRepository,
+            IMediator mediator)
+        {
+            _positionRepository = positionRepository;
+            _mediator = mediator;
+            _positionIds = new();
+        }
+
+        public async Task<Result<IError>> RegisterEmployeeAsync(BulkRegisterEmployee employee)
+        {
+            return await GetPositionIdAsync(employee.Position)
+                .BindAsync(positionId => _mediator.Send(new RegisterEmployeeCommand
+                {
+                    Email = employee.Email,
+
+                    Name = new()
+                    {
+                        FirstName = employee.FirstName,
+                        LastName = employee.LastName,
+                        MiddleName = employee.MiddleName,
+                    },
+
+                    PositionId = positionId,
+                }));
+        }
+
+        private async Task<Result<Guid, IError>> GetPositionIdAsync(string positionName)
+        {
+            if (_positionIds.TryGetValue(positionName, out var positionId))
+            {
+                return Result<Guid, IError>.Ok(positionId);
+            }
+
+            return await TryFetchPositionId(positionName)
+                .BindFailureAsync(async error => error is NoEntity ?
+                    await CreatePositionAndItsIdAsync(positionName) :
+                    Result<Guid, IError>.Fail(error));
+        }
+
+        private async Task<Result<Guid, IError>> TryFetchPositionId(string positionName)
+        {
+            return await _positionRepository
+                .FindByNameAsync(positionName)
+                .TeeAsync(position => _positionIds[position.Name] = position.Id)
+                .MapAsync(position => position.Id);
+        }
+
+        private async Task<Result<Guid, IError>> CreatePositionAndItsIdAsync(string positionName)
+        {
+            return await _mediator
+                .Send(new CreatePositionCommand
+                {
+                    Name = positionName,
+                    Type = PositionType.Employee
+                })
+                .TeeAsync(position => _positionIds[position.Name] = position.Id)
+                .MapAsync(position => position.Id);
+        }
+    }
+}
---
+++ src/KpiV3.WebApi/Converters/CsvConverter.cs
@@ -0,0 +1,39 @@
+﻿using CsvHelper;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace KpiV3.WebApi.Converters;
+
+public static class CsvConverter
+{
+    public static Result<List<T>, IError> Convert<T>(Stream csvStream)
+    {
+        using var reader = new StreamReader(csvStream);
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+        var records = csv.GetRecords<T>().ToList();
+
+        foreach (var record in records)
+        {
+            var context = new ValidationContext(record!);
+            var results = new List<ValidationResult>();
+
+            if (!Validator.TryValidateObject(record!, context, results, true))
+            {
+                return Result<List<T>, IError>.Fail(new InvalidInput(results.First().ErrorMessage!));
+            }
+        }
+
+        return Result<List<T>, IError>.Ok(records);
+    }
+
+    public static string Convert<T>(List<T> records)
+    {
+        var writer = new StringWriter();
+        var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+        csv.WriteRecords(records);
+
+        return writer.ToString();
+    }
+}
